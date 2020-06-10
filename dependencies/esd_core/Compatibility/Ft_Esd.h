cc16c03917803b356540a8010fb4588d1a83f4f4
Jan Boon <jan.boon@kaetemi.be>
2020-06-10 09:22:40 +0800

    Implementing ESD compatibility

--- dependencies/esd_core/Compatibility/Ft_Esd.h
+++ dependencies/esd_core/Compatibility/Ft_Esd.h
@@ -32,115 +32,9 @@
 #ifndef FT_COMPATIBILITY_ESD_H
 #define FT_COMPATIBILITY_ESD_H
 
-/*
-Type definition
-
-ESD_TYPE(ft_uint32_t, Native = UInt32, Edit = Int)
-
-ESD_TYPE(char *, Native = Latin1, Edit = String)
-
-ESD_TYPE(Esd_BitmapCell *, Native = Pointer, Edit = Library)
-
-typedef ft_int32_t ft_int32_f4_t;
-ESD_TYPE(ft_int32_f4_t, Native = Int32, Edit = Fixed4)
-
-*/
-
-#define ESD_TYPE(name, ...)
-#define ESD_SYMBOL(name, ...)
-
-/*
-Enum definition.
-Parses the enum identifiers, does not care about the integer or any other type of values
-
-Usage examples:
-
-ESD_ENUM(BitmapFormat, Type = ft_uint32_t)
-#define RGB 1
-#define ARGB 2
-ESD_END()
-
-ESD_ENUM(BitmapFormat)
-enum BitmapFormat
-{
-RGB = 1,
-ARGB
-};
-ESD_END()
-
-*/
-
-#define ESD_ENUM(name, ...)
-#define ESD_IDENTIFIER(name, ...)
-#define ESD_END()
-
-/* Node definition
-
-Usage examples:
-
-ESD_FUNCTION(testPrint, DisplayName = "Test Print", Icon = ":/icons/card-address.png")
-ESD_PARAMETER(message, Type = char *)
-void testPrint(char *message);
-
-ESD_FUNCTION(Ft_Gpu_Copro_SendCmd, Include = "FT_Gpu_Hal.h")
-ESD_PARAMETER(phost, Type = EVE_HalContext *, Default = "Ft_App_Get_Host()", Hidden, Internal)
-ESD_PARAMETER(cmd, Type = ft_uint32_t)
-
-*/
-
-#define ESD_FUNCTION(name, ...)
-#define ESD_METHOD(name, ...)
-#define ESD_UPDATE(name, ...)
-#define ESD_RENDER(name, ...)
-
-#define ESD_LOGIC(name, ...)
-#define ESD_ACTOR(name, ...)
-#define ESD_WIDGET(name, ...)
-#define ESD_PAGE(name, ...)
-#define ESD_APPLICATION(name, ...)
-
-#define ESD_PARAMETER(name, ...)
-
-#define ESD_SLOT(name, ...)
-#define ESD_SIGNAL(name, ...)
-#define ESD_INPUT(name, ...)
-#define ESD_OUTPUT(name, ...)
-#define ESD_VARIABLE(name, ...)
-#define ESD_WRITER(name, ...)
-
-#define ESD_CATEGORY(name, ...)
+#include "ESD_Core.h"
 
 #include "FT_Platform.h"
-
-ESD_TYPE(void, Native = Void)
-ESD_TYPE(char, Native = Char, Edit = String)
-ESD_TYPE(signed char, Native = Int8, Edit = Integer)
-ESD_TYPE(short, Native = Int16, Edit = Integer)
-ESD_TYPE(int, Native = Int32, Edit = Integer)
-ESD_TYPE(long, Native = Int32, Edit = Integer)
-ESD_TYPE(long long, Native = Int64, Edit = Integer)
-ESD_TYPE(unsigned char, Native = UInt8, Edit = Integer)
-ESD_TYPE(unsigned short, Native = UInt16, Edit = Integer)
-ESD_TYPE(unsigned int, Native = UInt32, Edit = Integer)
-ESD_TYPE(unsigned long, Native = UInt32, Edit = Integer)
-ESD_TYPE(unsigned long long, Native = UInt64, Edit = Integer)
-ESD_TYPE(float, Native = Float, Edit = Real)
-ESD_TYPE(double, Native = Double, Edit = Real)
-ESD_TYPE(_Bool, Native = Bool, Edit = Boolean)
-ESD_TYPE(char *, Native = Utf8, Edit = String)
-ESD_TYPE(const char *, Native = Utf8, Edit = String)
-ESD_TYPE(void *, Native = Pointer, Edit = None)
-
-ESD_TYPE(int8_t, Native = Int8, Edit = Integer)
-ESD_TYPE(uint8_t, Native = UInt8, Edit = Integer)
-ESD_TYPE(int16_t, Native = Int16, Edit = Integer)
-ESD_TYPE(uint16_t, Native = UInt16, Edit = Integer)
-ESD_TYPE(int32_t, Native = Int32, Edit = Integer)
-ESD_TYPE(uint32_t, Native = UInt32, Edit = Integer)
-ESD_TYPE(int64_t, Native = Int64, Edit = Integer)
-ESD_TYPE(uint64_t, Native = UInt64, Edit = Integer)
-// ESD_TYPE(ssize_t, Native = IntPtr, Edit = Integer)
-ESD_TYPE(size_t, Native = UIntPtr, Edit = Integer)
 
 ESD_TYPE(ft_char8_t, Native = Char, Edit = String)
 ESD_TYPE(ft_schar8_t, Native = Char, Edit = Integer)
@@ -192,14 +86,9 @@ ESD_TYPE(ft_rgb32_t, Native = UInt32, Edit = ColorRGB)
 typedef ft_uint32_t esd_classid_t;
 ESD_TYPE(esd_classid_t, Native = UInt32, Edit = Library)
 
-ESD_FUNCTION(Ft_Esd_Noop, Category = _GroupHidden)
+ESD_FUNCTION(Ft_Esd_Noop, Category = _GroupHidden, Macro)
 ESD_PARAMETER(context, Type = void *)
-void Ft_Esd_Noop(void *context);
-
-#define ESD_LOOPSTATE_NONE 0
-#define ESD_LOOPSTATE_IDLE 1
-#define ESD_LOOPSTATE_UPDATE 2
-#define ESD_LOOPSTATE_RENDER 3
+#define Ft_Esd_Noop ESD_Noop
 
 // TODO: This probably belongs in widgets framework, Ft_Esd.h is for core framework functionality only ->
 ESD_ENUM(Ft_AlignX_Opt, Type = ft_uint16_t)
@@ -214,32 +103,9 @@ ESD_ENUM(Ft_AlignY_Opt, Type = ft_uint16_t)
 ESD_END()
 // <-
 
-#ifdef ESD_SIMULATION
-// Functions provided by ESD environment
-void LogMessage__ESD(const char *str);
-void LogWarning__ESD(const char *str);
-void LogError__ESD(const char *str);
-#define Ft_Esd_LogMessage(s) LogMessage__ESD(s)
-#define Ft_Esd_LogWarning(s) LogWarning__ESD(s)
-#define Ft_Esd_LogError(s) LogError__ESD(s)
-#else
-#define Ft_Esd_LogMessage(s) \
-	do                       \
-	{                        \
-	} while (0)
-#define Ft_Esd_LogWarning(s) \
-	do                       \
-	{                        \
-	} while (0)
-#define Ft_Esd_LogError(s) \
-	do                     \
-	{                      \
-	} while (0)
-#endif
-
-#ifdef _MSC_VER
-#define inline __inline
-#endif
+#define Ft_Esd_LogMessage ESD_LogMessage
+#define Ft_Esd_LogWarning ESD_LogWarning
+#define Ft_Esd_LogError ESD_LogError
 
 /* compatibility */
 
@@ -250,8 +116,11 @@ void LogError__ESD(const char *str);
 #undef FT_DispHeight
 #endif
 
-#define FT_DispWidth ESD_DispWidth
-#define FT_DispHeight ESD_DispHeight
+#define FT_DispWidth phost->DispWidth
+#define FT_DispHeight phost->DispHeight
+
+#define ESD_DispWidth phost->DispWidth
+#define ESD_DispHeight phost->DispHeight
 
 #endif /* #ifndef FT_ESD_H */
 
