fab95a7acfbd3d7dfe0472f058648896d8893503
Jan Boon <jan.boon@kaetemi.be>
2020-06-12 05:22:28 +0800

    Compatibility macros

--- dependencies/esd_core/Compatibility/Ft_Esd_ResourceInfo.h
+++ dependencies/esd_core/Compatibility/Ft_Esd_ResourceInfo.h
@@ -11,102 +11,27 @@ Resource info structure
 #ifndef ESD_COMPATIBILITY_RESOURCEINFO_H
 #define ESD_COMPATIBILITY_RESOURCEINFO_H
 
+#include "ESD_Core.h"
 #include "Ft_Esd.h"
 #include "Ft_Esd_GpuAlloc.h"
 
-// Resource loading mechanisms
-ESD_ENUM(Esd_ResourceType, DisplayName = "Resource Type")
-#define ESD_RESOURCE_FILE 0
-#define ESD_RESOURCE_PROGMEM 1
-#define ESD_RESOURCE_FLASH 2
-#define ESD_RESOURCE_DIRECTFLASH 3
-ESD_END()
-
-ESD_ENUM(Esd_ResourceCompression, DisplayName = "Resource Compression")
-#define ESD_RESOURCE_RAW 0
-#define ESD_RESOURCE_DEFLATE 1
-#define ESD_RESOURCE_IMAGE 2
-ESD_END()
-
-// Both flash and direct flash have the same type bit set on purpose
-#define ESD_RESOURCE_IS_FLASH(resourceType) ((resourceType & ESD_RESOURCE_FLASH) == ESD_RESOURCE_FLASH)
-
 ESD_TYPE(Esd_ResourceInfo, Native = Struct)
-typedef struct Esd_ResourceInfo // (16 bytes) (24 bytes on 64 bit)
-{
-	// Source of data
-	union
-	{
-		const char *File;
-		ft_int32_t FlashAddress;
-		ft_prog_uchar8_t *ProgMem;
-	};
-
-	// (Runtime) Handle pointing to the address in RAM_G if it is allocated
-	Ft_Esd_GpuHandle GpuHandle;
-
-	// Size of data in storage, divided by 4, rounded up ((size + 3) >> 2) (size in int32)
-	uint32_t StorageSize : 27;
-
-	// (Esd_ResourceType) Source of data
-	uint32_t Type : 2;
-
-	// Load from compressed format
-	uint32_t Compressed : 2;
-
-	// When this is set, the allocated ram is not free'd automatically
-	// Use Ft_Esd_GpuAlloc_Free(GpuAlloc, GpuHandle) or Esd_FreeResource to free the GPU ram manually
-	uint32_t Persistent : 1;
-
-	// Size of data when decompressed into RAM_G
-	uint32_t RawSize;
-
-} Esd_ResourceInfo;
-
 ESD_TYPE(Esd_ResourceInfo *, Native = Pointer, Edit = Library)
+#define Esd_ResourceInfo ESD_ResourceInfo
 
-#if (EVE_SUPPORT_CHIPID >= EVE_FT810)
-#ifdef EVE_FLASH_AVAILABLE
-/// Transform flash address to BITMAP_SOURCE address parameter
-#define ESD_DL_FLASH_ADDRESS(address) ((address >> 5) | 0x800000)
-#endif
-/// Transform RAM_G address to BITMAP_SOURCE address paramter
-#define ESD_DL_RAM_G_ADDRESS(address) (EVE_CHIPID >= EVE_FT810 ? (address & 0x3FFFFF) : (address & 0xFFFFF))
-#else
-/// Transform RAM_G address to BITMAP_SOURCE address paramter
-#define ESD_DL_RAM_G_ADDRESS(address) (address & 0xFFFFF)
-#endif
-
-/// Flash address invalid
-#define FA_INVALID 0
-
-/// A function to load resource data into RAM_G (or to use the resource from flash directly)
-/// Returns address in the format as specified by the BITMAP_SOURCE command (see ESD_DL_FLASH_ADDRESS and ESD_DL_RAM_G_ADDRESS macros)
-/// Returns the output image format if the resource is an image loaded through the coprocessor
-uint32_t Esd_LoadResource(Esd_ResourceInfo *resourceInfo, ft_uint32_t *imageFormat);
-
-/// Free a currently loaded resource from RAM_G. Can be used to enforce reloading a resource.
-void Esd_FreeResource(Esd_ResourceInfo *resourceInfo);
+#define Esd_LoadResource ESD_LoadResource
+#define Esd_FreeResource ESD_FreeResource
 
 /// A function to make fonts persistent in memory by reloading the data if necessary, called during the Update cycle of each frame
-ESD_UPDATE(Esd_ResourcePersist, DisplayName = "Persist Resource", Category = EsdUtilities)
+ESD_UPDATE(Esd_ResourcePersist, DisplayName = "Persist Resource", Category = _GroupHidden, Macro)
 ESD_PARAMETER(resourceInfo, Type = Esd_ResourceInfo *)
-void Esd_ResourcePersist(Esd_ResourceInfo *resourceInfo);
+#define Esd_ResourcePersist ESD_ResourcePersist
 
 /// Number of available bitmap handles
-#define FT_ESD_BITMAPHANDLE_NB (EVE_CHIPID >= EVE_FT810 ? 32UL : 16UL)
-#if (EVE_SUPPORT_CHIPID >= EVE_FT810)
-#define FT_ESD_BITMAPHANDLE_CAP (32UL)
-#else
-#define FT_ESD_BITMAPHANDLE_CAP (16UL)
-#endif
-
-/// An invalid bitmap handle
-#define FT_ESD_BITMAPHANDLE_INVALID 0x3F
-#define FT_ESD_BITMAPHANDLE_VALID(bitmapHandle) (bitmapHandle < FT_ESD_BITMAPHANDLE_NB)
-
-/// Check if a format is an ASTC format
-#define ESD_IS_FORMAT_ASTC(format) ((format & 0xFFF0) == 0x93B0)
+#define FT_ESD_BITMAPHANDLE_NB ESD_BITMAPHANDLE_NB
+#define FT_ESD_BITMAPHANDLE_CAP ESD_BITMAPHANDLE_CAP
+#define FT_ESD_BITMAPHANDLE_INVALID ESD_BITMAPHANDLE_INVALID
+#define FT_ESD_BITMAPHANDLE_VALID ESD_BITMAPHANDLE_VALID
 
 #endif /* #ifndef ESD_FONTINFO_H */
 
